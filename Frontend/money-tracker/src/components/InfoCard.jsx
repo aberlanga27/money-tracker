376bc6f564473dd1cefdec87eaa4517d11b6861e
Armando Berlanga <armando.berlanga@slalom.com>
2025-05-12 11:40:17 -0600

    Refactor InfoCard component: update title and value styles for improved readability and consistency.

--- Frontend/money-tracker/src/components/InfoCard.jsx
+++ Frontend/money-tracker/src/components/InfoCard.jsx
@@ -3,8 +3,8 @@ import { currency } from "../utils/formatters";
 export function InfoCard({ title, value }) {
     return (
         <div className="grid grid-rows-2 bg-secondary/30 px-3 py-2 rounded-lg">
-            <h2 className="text-md font-bold text-gray-900">{title}</h2>
-            <span className="text-md font-bold bg-white text-primary p-1 rounded">{currency(value)}</span>
+            <h2 className="text-md font-bold text-primary">{title}</h2>
+            <span className="text-md font-bold bg-white p-1 rounded-md">{currency(value)}</span>
         </div>
     );
 }
